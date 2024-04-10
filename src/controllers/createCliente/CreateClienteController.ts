ac02e173f9b91c324b4179652237ec7fa53ef3c0
jose francisco <096791francis@gmail.com>
2024-04-10 20:16:00 -0300

    slkdjfla

--- src/controllers/createCliente/CreateClienteController.ts
+++ src/controllers/createCliente/CreateClienteController.ts
@@ -3,16 +3,9 @@ import { prisma } from "../../database/client";
 import { z, ZodError } from "zod";
 
 const clienteSchema = z.object({
-	nome: z.string().min(3).nonempty("Obrigatório"),
-	email: z.string().email({ message: "E-mail incorreto" }).nonempty("Obrigatório"),
-	cnpjcpf: z.string().nonempty("Obrigatório"),
-	telefone: z.string().nonempty("Obrigatório"),
-	celular: z.string().nonempty("Obrigatório"),
-	endereco: z.string().nonempty("Obrigatório"),
-	cep: z.string().nonempty("Obrigatório"),
-	razaosocial: z.string().nonempty("Obrigatório"),
-	empresaId: z.number(),
-	cidadeId: z.number()
+	consumidor: z.string(),
+	nunerro: z.string(),
+	datass: z.string(),
 });
 
 
@@ -20,46 +13,14 @@ type Cliente = z.infer<typeof clienteSchema>
 
 export const CreateClienteController = async (req: Request, res: Response,) => {
 	try {
-		const { nome, email, cnpjcpf, telefone, celular, endereco, cep, razaosocial, empresaId, cidadeId }: Cliente = clienteSchema.parse(req.body);
+		const { consumidor, nunerro, datass }: Cliente = clienteSchema.parse(req.body);
 
-		const cidadeExiste = await prisma.cidade.findUnique({
-			where: {
-				id: Number(cidadeId)
-			}
-		});
-
-		if (!cidadeExiste) {
-			return res.status(400).json({ message: "Essa Cidade Inexistente!" });
-		}
-
-		const empresaExiste = await prisma.empresa.findUnique({
-			where: {
-				id: Number(empresaId)
-			}
-		});
-
-		if (!empresaExiste) {
-			return res.status(400).json({ message: "Esse Empresa Inexistente!" });
-		}
-
-		const limparCnpj = cnpjcpf.replace(/[^a-zA-Z0-9]/g, "");
-		const limparTelefone = telefone.replace(/[^0-9]/g, "");
-		const limparCelular = celular.replace(/[^0-9]/g, "");
-		const limparCep = cep.replace(/[-]/g, "");
 
 		const cliente = await prisma.cliente.create({
 			data: {
-				nome,
-				email,
-				cnpjcpf: limparCnpj,
-				telefone: limparTelefone,
-				celular: limparCelular,
-				endereco,
-				cep: limparCep,
-				razaosocial,
-				empresaId,
-				cidadeId
-
+				consumidor,
+				nunerro,
+				datass
 			}
 		});
 		return res.status(200).json({ message: "Cliente Cadastrado", cliente });
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,76 +1,18 @@
 import { Router } from "express";
-import { CreateEmpresaController } from "../controllers/createEmpresa/CreateEmpresaController";
-import { ListandoEmpresaController } from "../controllers/createEmpresa/ListandoEpresaController";
-import { DeletarEmpresaController } from "../controllers/createEmpresa/DeletandoEmpresaController";
-import { AtualizandoEmpresaController } from "../controllers/createEmpresa/AtualizandoEmpresaController";
 import { CreateClienteController } from "../controllers/createCliente/CreateClienteController";
 import { ListandoClienteController } from "../controllers/createCliente/ListandoClienteController";
-import { ListandoUmaEmpresaController } from "../controllers/createEmpresa/ListandoUmaEmpresaController";
 import { ListandoUmClienteController } from "../controllers/createCliente/ListandoUmClienteController";
-import { AtualizandoClienteController } from "../controllers/createCliente/AtualizandoClienteController";
-import { DeletarClienteController } from "../controllers/createCliente/DeletandoClienteController";
-import { CreateOrcamentoController } from "../controllers/createOrcamento/CreateOrcamentoController";
-import { ListandoOrcametntoController } from "../controllers/createOrcamento/ListandoOrcamentoController";
-import { ListandoUmOrcamentoController } from "../controllers/createOrcamento/ListandoUmOrcamentoController";
-import { DeletarOrcamentoController } from "../controllers/createOrcamento/DeletandoOrcamentoController";
-import { AtualizandoOrcamentoController } from "../controllers/createOrcamento/AtualizandoOrcamentoController";
-import { CreateItemController } from "../controllers/createItem/CreateItemController";
-import { ListandoItemController } from "../controllers/createItem/ListandoItemController";
-import { ListandoUmItemController } from "../controllers/createItem/ListandoUmItemController";
-import { DeletarItemController } from "../controllers/createItem/DeletandoItemController";
-import { AtualizandoItemController } from "../controllers/createItem/AtualizandoItemController";
-import { CreateCidadeController } from "../controllers/createCidade/CreateCidadeController";
-import { CreateEstadoController } from "../controllers/createEstado/CreateEstadoController";
-import { ListandoCidadeController } from "../controllers/createCidade/ListandoCidadeController";
-import { ListandoUmaCidadeController } from "../controllers/createCidade/ListandoUmCidadeController";
-import { DeletarCidadeController } from "../controllers/createCidade/DeletandoCIdadeController";
-import { AtualizandoCidadeController } from "../controllers/createCidade/AtualizandoCidadeController";
-import { ListandoEstadoController } from "../controllers/createEstado/ListandoEstadoController";
-import { ListandoUmEstadoController } from "../controllers/createEstado/ListandoUmEstadoController";
-import { DeletarEstadoController } from "../controllers/createEstado/DeletandoCIdadeController";
-import { AtualizandoEstadoController } from "../controllers/createEstado/AtualizandoCidadeController";
-import { AuthAutorizaController } from "../controllers/authController/AuthAutorizaController";
-//import { AuthMiddleware } from "../controllers/middlewares/auth";
+
 
 const router = Router();
 
-router.post("/empresa",  CreateEmpresaController );
-router.get("/empresas",  ListandoEmpresaController );
-router.get("/empresa",  ListandoUmaEmpresaController );
-router.put("/empresa",  AtualizandoEmpresaController);
-router.delete("/empresa",  DeletarEmpresaController);
 
-router.post("/autenticar", AuthAutorizaController  );
 
 router.post("/cliente", CreateClienteController );
 router.get("/clientes", ListandoClienteController );
 router.get("/cliente", ListandoUmClienteController );
-router.put("/cliente", AtualizandoClienteController);
-router.delete("/cliente", DeletarClienteController);
 
-router.post("/orcamento", CreateOrcamentoController );
-router.get("/orcamentos", ListandoOrcametntoController );
-router.get("/orcamento", ListandoUmOrcamentoController );
-router.delete("/orcamento", DeletarOrcamentoController);
-router.put("/orcamento", AtualizandoOrcamentoController);
 
-router.post("/item", CreateItemController );
-router.get("/itens", ListandoItemController );
-router.get("/item", ListandoUmItemController );
-router.delete("/item", DeletarItemController);
-router.put("/item", AtualizandoItemController);
-
-router.post("/cidade", CreateCidadeController );
-router.get("/cidades", ListandoCidadeController );
-router.get("/cidade", ListandoUmaCidadeController );
-router.delete("/cidade", DeletarCidadeController);
-router.put("/cidade", AtualizandoCidadeController);
-
-router.post("/estado", CreateEstadoController );
-router.get("/estados", ListandoEstadoController );
-router.get("/estado", ListandoUmEstadoController );
-router.delete("/estado", DeletarEstadoController);
-router.put("/estado", AtualizandoEstadoController);
 
 
 export { router };

dcab40e2620029e06b6a6a994359decb8623e57c
yurieogrelo <yuri.eogrelo@gmail.com>
2024-10-20 17:36:32 -0300

    bora

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,10 @@ import cors from "cors";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-
 app.listen(PORT, () => {
-  console.log(`Server Rodando na Porta ${PORT}`);
+	console.log(`Server Rodando na Porta ${PORT}`);
 });
